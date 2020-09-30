c02327481c68e1057db7482b5efa54cb689719f0
Hafizoune <mamadouhafizka@gmail.com>
2020-09-30 16:35:26 +0000

    initiation it4life project

---
+++ it4life_academy/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "academy",
+
+    'summary': """School trainings""",
+
+    'description': """
+        Odoo school module for school trainings:
+            - training courss
+    """,
+
+    'author': "Hafiz",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [ 
+        # 'views/schoolmodule.xml',
+           
+    ],
+
+}
